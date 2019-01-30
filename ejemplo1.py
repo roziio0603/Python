8bddc28c087f30d72be34315ccfee860967075e6
roziio0603 <rocio62003@gmail.com>
2019-01-30 17:03:26 -0600

    ejemplo con if y else

---
+++ ejemplo1.py
@@ -0,0 +1,14 @@
+##Rocio
+##Calcular la edad actual de una persona, previamente ingresando el año actual y el año de nacimiento
+
+print("Bienvenido al programa".center(50,"*"))
+FEC_ACT = 2019
+fec_nac = int(input("ingresa tu anio de nacimiento"))
+
+edad = FEC_ACT - fec_nac
+print("Tu edad es {}".format(edad))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
