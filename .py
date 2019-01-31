60b99fb08266245a65f2d8580a8b07170e5eb3e0
roziio0603 <rocio62003@gmail.com>
2019-01-30 23:32:38 -0600

    Ejercicios correctos

--- .py
+++
@@ -1,4 +0,0 @@
-num1 = input("Valor numero1")
-num2 = input("Valor numero2")
-suma = num1 + num2
-print("La suma es {}".format(suma))
--- ejercicios.py
+++ ejercicios.py
@@ -4,70 +4,52 @@
 ##Convertir dolares a quetzales
 ##quetzales a dolares
 
-##menu = '''Menu
-##        1.Convertir dolares a quetzales
-##        2.Convetir quetzales a dolares
-##        3.Salir
-##        '''
-##print(menu)
-##opcion = input(":.")
-##while opcion != 3:
-##    if opcion == 1:
-##        quetzal = float(input("Valor a convertir"))
-##        TASA = 7.5
-##        dolares = quetzal // 7.5
-##        print("La cantidad es{dolares}".format(dolares))
-##    elif opcion == 2:
-##        dolares = float(input("Valor a convertir"))
-##        quetzal = dolares * 7.5
-##        print("La cantidad es{quetzal}".format(quetzal))
-##    else:
-##        print("Opcion no valida")
-##        print(menu)
-##    opcion = input(":.")
+dolar = 7.5
+print ("Opciones")
+print ("1. Convertir dolares a quetzales")
+print ("2. Convertir quetzales a dolares")
+opcion = input("Ingrese la opcion que desee realizar")
+
+if opcion == 1:
+    cantidad = input("Ingrese cantidad")
+    print ("resultado")
+    print (cantidad * dolar)
+elif opcion == 2:
+    cantidad = input("Ingrese cantidad")
+    print ("resultado")
+    print (cantidad / dolar)
+else:
+    print ("opcion no valida")
 
 ##Solicitar al usuario que escoga una de las siguientes opciones
 ##Sumar dos numeros, restar tres numeros, multiplicar cuatro numeros, dividir dos numeros.
 ##si el usuario elegi una opcion invalida hacerselo saber
 
-menu = '''Menu
-        1. Suma
-        2. Resta
-        3. Multiplicacion
-        4. Division
-        5. Salir
-        '''
-print (menu)
-opcion = input(":.")
-while opcion != 5:
-    if menu == 1:
-        num1 = input("Valor numero1")
-        num2 = input("Valor numero2")
-        suma = num1 + num2
-        print("La suma es {}".format(suma))
-    elif opcion == 2:
-        num1 = input("Valor numero1")
-        num2 = input("Valor numero2")
-        num3 = input("Valor numero3")
-        resta = num1 - num2 - num3
-        print("La resta es {}".format(resta))
-    elif opcion == 3:
-        num1 = input("Valor numero1")
-        num2 = input("Valor numero2")
-        num3 = input("Valor numero3")
-        num4 = input("Valor numero4")
-        multiplicacion = num1 * num2 * num3 * num4
-        print("La multiplicacion es {}".format(multiplicacion))
-    elif opcion == 4:
-        num1 = input("Valor numero1")
-        num2 = input("Valor numero2")
-        division = num1 // num2
-        print("La division es {}".format(division))
-    else:
-        print("Opcion no valida")
-    print (menu)
-    opcion = input(":.")
-print ("FIN")
-        
-        
-        
+
+##print ("Operacion a realizar")
+##print ("1)suma ")
+##print ("2)resta")
+##print ("3)multiplicacion ")
+##print ("4)division")
+##
+##opcion = input("Elija una operacion")
+##valor1 = int(input("Ingrese numero"))
+##valor2 = int(input("Ingrese numero"))    
+##valor3 = int(input("Ingrese numero"))     
+##valor4 = int(input("Ingrese numero"))
+##if opcion  == "1":
+##    suma = valor1 + valor2
+##    print ("La suma es \n {}".format(suma))
+##
+##elif opcion == "2":
+##    resta = valor1 - valor2 - valor3
+##    print ("La resta es \n {}".format(resta))
+##
+##elif opcion == "3":
+##    multiplicacion = valor1 * valor2 * valor3 * valor4
+##    print ("La multiplicacion es \n {}".format(multiplicacion))
+##
+##elif opcion == "4":
+##    multiplicacion = valor1 / valor2
+##    print ("La division es \n {}".format(division))
+##    
